503a704183979bd10e7fd2f5dabeb7ad09cc06c9
lara barbosa <larapattylprb@gmail.com>
2021-05-24 17:10:18 -0300

    new_view working

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,13 @@
+class GamesController < ApplicationController
+  def new
+    @letters = params[:letters]
+    @letters = []
+  10.times do 
+      @letters << ('a'..'z').to_a.sample
+    end
+  end
+
+  def score
+    
+  end
+end
